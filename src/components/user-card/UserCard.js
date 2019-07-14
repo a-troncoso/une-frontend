4fc87841243b97d7e8e35356d0c1a7438c3a52a6
Alvaro Troncoso <alvaro.mc2@gmail.com>
2019-07-14 12:39:17 -0400

    formatting and styling userCard component

--- src/components/user-card/UserCard.js
+++ src/components/user-card/UserCard.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image} from 'react-native';
+import { Icon } from 'react-native-elements';
 import PropTypes from 'prop-types';
 
 class UserCard extends Component {
   render() {
     return (
-      <View>
-        <Text>This is user Information Page</Text> 
-        <Text>This is detail of user: {this.props.user.name}</Text>
-        <Text>ID: {this.props.user.id}</Text>
-        <Text>Image: {this.props.user.imageUrl}</Text>       
+      <View style={{flex:1}}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={{uri: this.props.user.portraitImageUrl}}
+            style={{height: 150}} />
+            <Text style={styles.placeName}>{this.props.user.name}</Text>
+          </View>
+          <View style={styles.infoContainer}>
+            <View style={{flex:1}}>
+              <Text style={{fontSize: 16, color: '#000'}}>
+                {this.props.user.description}
+              </Text>
+            </View>
+            <View style={styles.likes}>
+              <Icon name='favorite-border' type="material" />
+              <Text style={{marginLeft: 5}}>{this.props.user.likes}</Text>
+            </View>
+          </View>
       </View>
     );
   }
@@ -19,5 +33,39 @@ UserCard.propTypes = {
   id: PropTypes.string,
   user: PropTypes.object.isRequired
 }
+
+const styles = StyleSheet.create({
+  infoContainer: {
+    flexDirection: 'row',
+    alignContent: 'flex-end',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#d3d3d3'
+  },
+  imageContainer: {
+    position: 'relative'
+  },
+  placeName: {
+    position: 'absolute',
+    bottom: 2,
+    left: 10,
+    color: '#fff',
+    fontSize: 25,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    textShadowOffset: {
+      width: 2,
+      height: 2
+    },
+    textShadowColor: '#000',
+    textShadowRadius: 10
+  },
+  likes: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    // borderWidth: 0.5,
+    // borderColor: '#d3d3d3'
+  }
+})
 
 export default UserCard;
--- src/pages/sellers/Sellers.js
+++ src/pages/sellers/Sellers.js
@@ -7,30 +7,44 @@ const sellers = [{
   imageUrl: 'https://acustiknoticias.com/wp-content/uploads/2018/12/libros.png',
   name: 'Teresa Machuca',
   description: 'libros para grandes y chicos',
-  portraitImageUrl : 'https://acustiknoticias.com/wp-content/uploads/2018/12/libros.png'
+  portraitImageUrl : 'https://acustiknoticias.com/wp-content/uploads/2018/12/libros.png',
+  likes: 7
 }, {
   id: '2',
   imageUrl: 'https://cdn.mindful.org/Meditation_Goleman.jpg',
   name: 'Camila Rincón',
   description: 'Libros para bebés',
-  portraitImageUrl : 'https://acustiknoticias.com/wp-content/uploads/2018/12/libros.png'
+  portraitImageUrl : 'https://acustiknoticias.com/wp-content/uploads/2018/12/libros.png',
+  likes: 5
 }, {
   id: '3',
   imageUrl: 'http://www.perpetualpc.com.au/c/wp-content/uploads/2012/07/sustainability.jpg',
-  name: 'Repostería vegana hecha con amor',
-  description: 'asd',
-  portraitImageUrl : 'https://acustiknoticias.com/wp-content/uploads/2018/12/libros.png'
+  name: 'Miranda Carrasco',
+  description: 'Repostería vegana hecha con amor',
+  portraitImageUrl : 'https://acustiknoticias.com/wp-content/uploads/2018/12/libros.png',
+  likes: 2
 }, {
   id: '4',
   imageUrl: 'https://acustiknoticias.com/wp-content/uploads/2018/12/libros.png',
   name: 'Celina Gómez',
   description: 'Plantas medicinales para que cultives en casa',
-  portraitImageUrl : 'https://acustiknoticias.com/wp-content/uploads/2018/12/libros.png'
+  portraitImageUrl : 'https://acustiknoticias.com/wp-content/uploads/2018/12/libros.png',
+  likes: 1
 }];
 
 class SellersPage extends Component {
 
   getSellers = () => sellers;
+
+  _renderItem = ({item, idx}) => {
+    return (
+      <View style={{flex: 1}}>
+        <TouchableOpacity activeOpacity={0.7} onPress={() => this._onPressSeller(item)}>
+          <UserCard id={item.id} user={item} />
+        </TouchableOpacity>
+      </View>
+    )
+  }
 
   _keyExtractor = item => item.id;
 
@@ -42,15 +56,11 @@ class SellersPage extends Component {
     return (
       <View style={{flex: 1}}>
         <FlatList
+          style={{flex: 1}}
           data={this.getSellers()}
-          renderItem={({item}) => (
-            <View>
-              <TouchableOpacity activeOpacity={0.7} onPress={() => this._onPressSeller(item)}>
-                <UserCard id={item.id} user={item} />
-              </TouchableOpacity>
-            </View>
-          )}
+          renderItem={this._renderItem}
           keyExtractor={this._keyExtractor}
+          numColumns={2}
         />
       </View>
     );
